71d73d1a6fd4f7fe5b83931d750238c568fe0e35
Omar Gomez <og922318@gmail.com>
2022-02-15 12:52:07 -0600

    Cambios hechos

--- kiosko/Models/KioskoCmsContext.cs
+++ kiosko/Models/KioskoCmsContext.cs
@@ -25,7 +25,7 @@ namespace kiosko.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=LAPTOP-KEIOQ6D6\\SQLEXPRESS;Database=KioskoCms;Trusted_Connection=true;user=sa;password=colimasoft;");
+                optionsBuilder.UseSqlServer("Server=LAPTOP-N1U5HNVN\\SQLEXPRESS;Database=KioskoCms;Trusted_Connection=true;");
             }
         }
 
