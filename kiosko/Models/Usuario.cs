e3956fe5c1b23ad414cee592d620afc83d7607fe
juanrivera-cs <juan.rivera@colimasoft.com>
2022-03-28 11:52:21 -0600

    configuraciones modulos

--- kiosko/Models/Usuario.cs
+++ kiosko/Models/Usuario.cs
@@ -7,7 +7,7 @@ namespace kiosko.Models
     {
         public int Id { get; set; }
         public int IdUsuario { get; set; }
-        public string Usuario1 { get; set; } = null!;
+        public string NombreUsuario { get; set; } = null!;
         public string? Clave { get; set; }
         public string? Rol { get; set; }
         public byte[]? Email { get; set; }
