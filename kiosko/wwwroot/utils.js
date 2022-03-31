6dd214c50ef6ecd27be8079ca63a07b7ed5bf75a
Ulises <ulises.puente@colimasoft.com>
2022-03-31 13:24:34 -0600

    Valores de alertas

--- kiosko/wwwroot/utils.js
+++ kiosko/wwwroot/utils.js
@@ -25,7 +25,6 @@ function getAllModulos() {
             addButtonToMenu();
             addButtonsToSubMenu();
             $("#modulo" + idCurremtModulo).children("a").addClass("active");
-            console.log(idCurremtModulo);
         }
     });
 }
