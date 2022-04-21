62478efb65b19474c697ed7e9c875cc3fb1bc94c
Erick Fco <erick.barreto@colimasoft.com>
2022-04-21 10:28:29 -0500

    Api para consumo de la app del modulo de avisos iniciales

---
+++ kiosko/Models/AvisoInicial.cs
@@ -0,0 +1,10 @@
+﻿namespace kiosko.Models
+{
+    public class AvisoInicial
+    {
+        public int Id { get; set; }
+        public string tipoComponente { get; set; }
+        public string url { get; set; }
+        public string descripcion { get; set; }
+    }
+}
